93ce95235e5836bc1d111f7e78f91adfe8fc799e
Oleksandr Akhriutin <oleksandr.akhriutin@crosslend.com>
2017-02-24 22:13:16 +0200

    app setup

---
+++ src/server/server.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let webpackDevMiddleware = require('webpack-dev-middleware');
+let webpack = require('webpack');
+let webpackConfig = require('../../webpack.config');
+
+let app = express();
+let compiler = webpack(webpackConfig);
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: '/'
+}));
+
+app.get('*', function response(req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+module.exports = {
+  context: __dirname + '/src',
+  entry: './client/index.js',
+  devtool: 'eval-source-map',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [/node_modules/],
+        query: {
+          presets: ['es2015']
+
+        }
+      }, {
+        test: /\.html$/,
+        loader: 'raw'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+};
