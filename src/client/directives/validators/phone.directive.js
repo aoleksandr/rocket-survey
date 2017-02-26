92119aa34a1f4592b89f543ba05a894176d18e3e
Oleksandr Akhriutin <oleksandr.akhriutin@crosslend.com>
2017-02-27 00:03:55 +0200

    phone number formatting

--- src/client/directives/validators/phone.directive.js
+++ src/client/directives/validators/phone.directive.js
@@ -2,12 +2,32 @@ export default function () {
   let directive = {
     require: 'ngModel',
     link(scope, elm, attrs, ctrl) {
+
+      ctrl.$parsers.push(input => {
+        // phone format: (000) 000-0000
+        let phoneFillers = {
+          '0': '(',
+          '3': ') ',
+          '6': '-'
+        };
+        let phoneNumber = input.replace(/[^\d]/g, '').substring(0, 10);
+        let formatted = '';
+
+        for (let i = 0; i < phoneNumber.length; i++) {
+          formatted += (phoneFillers[i] || '') + phoneNumber[i];
+        }
+        ctrl.$setViewValue(formatted);
+        ctrl.$render();
+
+        return formatted;
+      });
+
       ctrl.$validators.phone = function (modelValue, viewValue) {
         if (ctrl.$isEmpty(modelValue)) {
           return true;
         }
-        //todo: add a formatter here
-        return !!(/^123$/.test(viewValue));
+        
+        return viewValue.length === 14;
       };
     }
   };
